837c4021bff1b318f4f88739404e004f1f9e6df6
Matthew Catellier <mattcatellier@gmail.coml>
2016-03-15 13:26:35 -0700

    Added in ability to make colors lighter and darker based on percentage

--- MakeColor/ViewController.swift
+++ MakeColor/ViewController.swift
@@ -14,21 +14,29 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
+        self.view.backgroundColor = UIColor.whiteColor();
+        
         let mk = MakeColor();
         // SOLID COLOR
-        // self.view.backgroundColor = mk.makeColor("FF0000");
+        var color = mk.makeColor("FF5E3A")// mk.makeOpac(, alpha: 0.8);
         
-        // VERTICAL GRADIENT
-        // let gradient = mk.makeGradient("FF0000", color2: "0000FF")
-        // gradient.frame = view.bounds;
-        // self.view.layer.insertSublayer(gradient, atIndex: 0)
+        color = mk.makeLighter(color, percent: 30)
+        // color = mk.makeDarker(color, percent: 20)
+        self.view.backgroundColor = color;
+
         
-        // HOIZONTAL GRADIENT
-        let startPoint = CGPoint(x: 0, y: 0.8);
-        let endPoint = CGPoint(x: 1, y: 0.2)
-        let gradient2 = mk.makeGradient("FF00000", color2: "0000FF", startPoint: startPoint, endPoint: endPoint)
-        gradient2.frame = view.bounds;
-        self.view.layer.insertSublayer(gradient2, atIndex: 0)
+         // VERTICAL GRADIENT
+         let gradient = mk.makeVerticalGradient("FF9500", color2: "FF5E3A")
+         gradient.frame = view.bounds;
+         self.view.layer.insertSublayer(gradient, atIndex: 0)
+        
+//        // HOIZONTAL GRADIENT
+//        let startPoint = CGPoint(x: 0, y: 0.8);
+//        let endPoint = CGPoint(x: 1, y: 0.2)
+//        var gradient2 = mk.makeGradient("FF00000", color2: "0000FF", startPoint: startPoint, endPoint: endPoint)
+//        gradient2.frame = view.bounds;
+//        gradient2 = mk.makeGradientOpac(gradient2, alpha: 0.8);
+//        self.view.layer.insertSublayer(gradient2, atIndex: 0)
     }
 
     override func didReceiveMemoryWarning() {
@@ -36,6 +44,7 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
 }
+
+
 
